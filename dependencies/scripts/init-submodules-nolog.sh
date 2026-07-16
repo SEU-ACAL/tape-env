a5d6677dc4a580b71c0a7103de07a6ccc7f7477f
Guo Bing <93064847+out-of-order55@users.noreply.github.com>
2026-07-16 21:05:02 +0800

    submodule(firemarshal): remove FireMarshal submodule (#16)
    * submodule(firemarshal): remove FireMarshal submodule
    
    * ci(firemarshal): remove obsolete initialization
    
    * ci(firemarshal): restore initialization
    
    * ci(submodules): initialize regression workspaces

--- dependencies/scripts/init-submodules-nolog.sh
+++ dependencies/scripts/init-submodules-nolog.sh
@@ -196,7 +196,6 @@ cd "$RDIR"
             soc-generator/sims/firesim \
             applications/nvdla-workload \
             applications/coremark \
-            applications/firemarshal \
             applications/spec2017 \
             applications/zephyr \
             dependencies/tools/dsptools \
@@ -309,12 +308,4 @@ cd "$RDIR"
     submodule_name="dependencies/tools/dsptools"
     git submodule update --init dependencies/tools/dsptools || exit 1
 
-    # Only shallow clone needed for basic SW tests
-    submodule_name="applications/firemarshal"
-    git submodule update --init applications/firemarshal || exit 1
 )
-
-# Configure firemarshal to know where our firesim installation is
-if [ ! -f ./applications/firemarshal/marshal-config.yaml ]; then
-  echo "firesim-dir: '../../soc-generator/sims/firesim/'" > ./applications/firemarshal/marshal-config.yaml
-fi
